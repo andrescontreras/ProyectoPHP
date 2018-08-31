7d1051fd1d31cff24fb8a1b806a7f2ccefa4c506
4khaos <imroocker@gmail.com>
2018-08-31 12:42:10 -0500

    new

--- Banco/login.php
+++ Banco/login.php
@@ -10,6 +10,14 @@
     <p>MUERTE A VS CODE</p>
     <h1>fgdsakjgfkuas gk</h1>
     <h2>cambio33333333</h2>
-    <h3>cambio 44444444 </h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+       irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+         cupidatat non proident, sunt in culpa qui
+      officia deserunt mollit anim id est laborum.</p>
+      <h1>fdsaghfdsfdsfgadsj fdsga hjkf fdasjf ghj adsfj</h1>
   </body>
 </html>
