747a160253c9595477258d22f63c6fba6ceb2557
ssalamanca <santiago.anime@hotmail.com>
2018-09-02 14:59:45 -0500

    Funciona enviar correo

--- controlador/c_correo.php
+++ controlador/c_correo.php
@@ -1,24 +1,23 @@
 <?php
 require_once ('../PHPMailer/PHPMailerAutoload.php');
+class correo{
+    public static function enviarCorreo($correo,$subject,$mensaje){
+            echo "Se va a enviar el correo";
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->SMTPAuth=true;
+            $mail->SMTPSecure='ssl';
+            $mail->Host='smtp.gmail.com';
+            $mail->Port= '465';
+            $mail->isHTML();
+            $mail->Username='prowebphp@gmail.com';
+            $mail->Password='php12345';
+            $mail->SetFrom('no-reply@proweb.org');
+            $mail->Subject= $subject;
+            $mail->Body= $mensaje;
+            $mail->AddAddress($correo);
+            $mail->Send();
+    } 
+}
         
-        echo "Se creo la persona";
-        $correo = $_POST["correo"];
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->SMTPAuth=true;
-        $mail->SMTPSecure='ssl';
-        $mail->Host='smtp.gmail.com';
-        $mail->Port= '465';
-        $mail->isHTML();
-        $mail->Username='prowebphp@gmail.com';
-        $mail->Password='php12345';
-        $mail->SetFrom('no-reply@proweb.org');
-        $mail->Subject='Registro completo';
-        $mail->Body='Se registro la persona al sistema';
-        $mail->AddAddress($correo);
-        $mail->Send();
-       
-        // enviar correo
-        echo "Para acceder al gestion de archivos copie en su navegador lo siguiente: http://localhost/gestor.html";   
-
 ?>
