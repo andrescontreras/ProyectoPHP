8541caedca730bdb4b51f2dcb6baf3579498d8d2
AndresContreras <andres_c8@outlook.com>
2018-09-02 16:19:59 -0500

    conrolador fin de mes

--- controlador/c_adminFinMes.php
+++ controlador/c_adminFinMes.php
@@ -1,3 +1,15 @@
 <?php
 // en este archivo se realiza toda la logica para hacer el fin de mes del banco
+class c_adminFinMes
+{
+    public function __construct()
+    {
+
+    }
+
+    public function finMes()
+    {
+        
+    }
+}
 ?>
