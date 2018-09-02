0d39163ad4672bb8393c2b87f19935bca8ceaf5a
4khaos <imroocker@gmail.com>
2018-09-02 17:47:21 -0500

    admon

--- controlador/c_adminFinMes.php
+++ controlador/c_adminFinMes.php
@@ -1,3 +1,28 @@
 <?php
-// en este archivo se realiza toda la logica para hacer el fin de mes del banco
+  include_once("../modelo/m_finMes.php");
+  include_once("../modelo/m_usuario.php");
+  include_once("../modelo/m_credito.php");
+  class c_adminFinMes
+  {
+    //Inicia la operación de fin de mes
+    public static function finDeMes()
+    {
+      c_adminFinMes::cobrarCreditos();
+      c_adminFinMes::cobrarTarjetas();
+    }
+    //cobra los créditos
+    public static function cobrarCreditos()
+    {
+      $consulta = m_usuario::getUsuarios();
+      while ($fila = mysqli_fetch_array($consulta))
+      {
+        
+      }
+    }
+    //Cobra las tarjetas de crédito
+    public static function cobrarTarjetas()
+    {
+
+    }
+  }
 ?>
